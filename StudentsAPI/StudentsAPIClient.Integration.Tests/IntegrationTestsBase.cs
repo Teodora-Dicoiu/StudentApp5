a7e63088d75e02d79c3ba986f257e31063e99ae6
teodora.dicoiu <teodora.dicoiu@fortech.ro>
2020-01-17 16:43:06 +0200

    first commit

---
+++ StudentsAPI/StudentsAPIClient.Integration.Tests/IntegrationTestsBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace StudentsAPIClient.Integration.Tests
+{
+    public class IntegrationTestsBase: IClassFixture<WebApplicationFactory<Startup>>
+    {
+        WebApplicationFactory<Startup> factory;
+        protected readonly HttpClient client ;
+
+        public IntegrationTestsBase(WebApplicationFactory<Startup> factory)
+        {
+            this.client = factory.CreateClient();
+            client.BaseAddress = new Uri("https://localhost:44363/");
+            client.DefaultRequestHeaders.Add("api-version", "2.0");
+            client.DefaultRequestHeaders.Add("x-api-key", "123456");
+            this.factory = factory;
+        }
+
+    }
+}
---
+++ StudentsAPI/StudentsAPIClient.Integration.Tests/StudentsStatisticsControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using StudentsAPIClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StudentsAPIClient.Integration.Tests
+{
+    public class StudentsStatisticsControllerTests : IntegrationTestsBase
+    {
+
+        public StudentsStatisticsControllerTests(WebApplicationFactory<Startup> factory) : base(factory)
+        {
+
+        }
+
+        [Fact]
+        async Task TestGetStudentsStatisticsStatus()
+        {
+            var response = await client.GetAsync("clientapi/StudentsStatistics");
+            response.EnsureSuccessStatusCode();
+      
+
+        }
+
+        [Fact]
+        async Task TestGetStudentsStatistics()
+        {
+            var response = await client.GetAsync("clientapi/StudentsStatistics");
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            List<StudentStatistics> students = await JsonSerializer.DeserializeAsync<List<StudentStatistics>>(responseStream);
+            Assert.Equal(5, students.Count);
+        }
+    }
+}
